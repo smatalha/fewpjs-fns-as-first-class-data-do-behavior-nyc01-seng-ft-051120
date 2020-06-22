1c891c9785dcc0960b369a0f8e69ede563473daa
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-06-22 23:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
-
 function greet() {
 
 }
+/* Write your implementation of displayMessage() */
 
-function displayMessage() {
-
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message
 }
