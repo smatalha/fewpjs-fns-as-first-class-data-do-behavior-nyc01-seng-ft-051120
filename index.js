3a885b1f347006238037479797149ebf1f40228a
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-06-23 00:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet() {
-
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if ( hour < 12) return "Good Morning"
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
 }
 /* Write your implementation of displayMessage() */
 
